c139b6421427eed9ba6fb8a82479270517f0d5c9
ljyxy1997 <ljyxy1997@163.com>
2021-04-27 20:33:31 +0800

    class3 end

--- 4.27/class3-while循环和for循环.py
+++ 4.27/class3-while循环和for循环.py
@@ -1,3 +1,16 @@
-a=1
-b=2
-c=a+b#try
+condition=1
+while condition<5:
+    print(condition)
+    condition+=1
+
+for i in range(5):
+    print(i)
+
+for p in range(2,10):
+    print(p)
+
+for u in range(1,10,2):
+    print(u)
+
+for t in [1,3,5,7]:
+    print(t)
