c6d904af1e32a4c3708200fe156a39e8a639dc55
ljyxy1997 <ljyxy1997@163.com>
2021-04-29 20:18:09 +0800

    class6

---
+++ 4.29/class6-元组.py
@@ -0,0 +1,10 @@
+a_tuple=(1,2,30,30,4,5)
+b_tuple=1,2,30,30,4,5
+print(a_tuple)
+print(b_tuple)
+print(a_tuple[1])
+print(a_tuple[1:4])
+print(a_tuple.count(30))
+for i in a_tuple:
+    print(i)             #元组与列表最大区别是元组里面的内容不能修改
+
