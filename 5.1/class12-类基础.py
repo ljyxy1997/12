eb43af2e3e80a145218db008c105752386fedba0
ljyxy1997 <ljyxy1997@163.com>
2021-05-01 16:40:43 +0800

    class12

---
+++ 5.1/class12-类基础.py
@@ -0,0 +1,40 @@
+class human: #类
+    #类的属性
+    name ='someone'
+    age =100
+    #类的方法
+    def my_name(self):
+        print('my name is',self.name)
+    def my_age(self):
+        print('my age is',self.age)
+    def eat(self):
+        print('eat')
+    def think(self,a,b):
+        print(a+b)
+person1 =human() #创建一个person1的对象
+print(person1.name)
+person1.name='zhangsan'
+print(person1.name)
+print(person1.think(1,2))
+
+class human1: #类
+    def __init__(self,name,age): #创建对象时会执行
+        self.name=name
+        self.age=age
+    #类的属性
+    name ='someone'
+    age =100
+    #类的方法
+    def my_name(self):
+        print('my name is',self.name)
+    def my_age(self):
+        print('my age is',self.age)
+    def eat(self):
+        print('eat')
+    def think(self,a,b):
+        print(a+b)
+
+person2 =human1('xiaoming',10)
+print(person2.name)
+print(person2.my_name())
+print(person2.age)
