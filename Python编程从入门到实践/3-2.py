64fda659901d1c6f2c26caff6979e97d9c733fa4
ljyxy1997 <ljyxy1997@163.com>
2021-05-02 21:27:46 +0800

    exercise

---
+++ Python编程从入门到实践/3-2.py
@@ -0,0 +1,5 @@
+names=['xiaoming','xiaohong','xiaolan','xiaomi']
+print(f"{names[0]} nice to meet you!")
+print(f"{names[1]} nice to meet you!")
+print(f"{names[2]} nice to meet you!")
+print(f"{names[3]} nice to meet you!")
