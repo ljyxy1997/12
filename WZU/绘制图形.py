cf53c5d53bba712b01308b0743c452bcdc9fa8d2
ljyxy1997 <ljyxy1997@163.com>
2021-05-16 17:46:56 +0800

    绘制图形的三种方法，推荐使用第三种

--- WZU/绘制图形.py
+++ WZU/绘制图形.py
@@ -10,9 +10,17 @@ cols=data.shape[1]
 x=data.iloc[:,1:2]
 print(x)
 y=data.iloc[:,-1:]
+y=data.iloc[:,-1:]
 data.plot(kind='scatter', x='x1', y='y', figsize=(12,8)) #第一种方法。注意是data.plot而不是plt.plot,就算已经设定了x，y的值也不行，里面的x、y需要设定你想表示的数值，kind:图的种类，figsize(a,b):a为X轴长，b为Y轴长
 plt.show()
+
 plt.scatter(x, y)#第二种方法。括号前是图的种类
 plt.xlabel('人口',fontsize=18) #frontsize:人口字体大小
 plt.ylabel('收益', rotation=0, fontsize=18) #rotation:收益字体的旋转角度
+plt.show()
+
+fig, ax = plt.subplots(figsize=(12, 8))  #第三种方法,这种方法较好
+ax.scatter(x,y,s=10,c='b',marker='*',label='negative')
+ax.scatter(x,y-1,s=50,c='r',marker='o',label='postive')
+plt.legend()
 plt.show()
