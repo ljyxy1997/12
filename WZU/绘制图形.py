0c9af1b209a655c154082877ffe210400e18fa47
ljyxy1997 <ljyxy1997@163.com>
2021-05-16 17:58:20 +0800

    绘制图形的三种方法，数据已提取推荐使用第三种,未提取使用第一种

--- WZU/绘制图形.py
+++ WZU/绘制图形.py
@@ -11,9 +11,9 @@ x=data.iloc[:,1:2]
 print(x)
 y=data.iloc[:,-1:]
 y=data.iloc[:,-1:]
-data.plot(kind='scatter', x='x1', y='y', figsize=(12,8)) #第一种方法。注意是data.plot而不是plt.plot,就算已经设定了x，y的值也不行，里面的x、y需要设定你想表示的数值，kind:图的种类，figsize(a,b):a为X轴长，b为Y轴长
-plt.show()
-
+data.plot(kind='scatter', x='x1', y='y', figsize=(12,8)) #第一种方法。注意是data.plot而不是plt.plot.
+plt.show()                                               #就算已经设定了x，y的值也不行，里面的x、y需要设定你想表示的数值的标题，kind:图的种类，figsize(a,b):a为X轴长，b为Y轴长
+                                                         #该方法适合绘制只知道那一列数据的标题,并没有将那一列数据提取出来的情况
 plt.scatter(x, y)#第二种方法。括号前是图的种类
 plt.xlabel('人口',fontsize=18) #frontsize:人口字体大小
 plt.ylabel('收益', rotation=0, fontsize=18) #rotation:收益字体的旋转角度
