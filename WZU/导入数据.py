677076620fca1b87505bac5176b9b0fc690dbf4f
ljyxy1997 <ljyxy1997@163.com>
2021-05-16 10:32:13 +0800

    导入数据的几种方式

--- WZU/导入数据.py
+++ WZU/导入数据.py
@@ -1,5 +1,16 @@
 import pandas as pd
 import numpy as np
-path = 'data/regress_data1.csv'
-data = pd.read_csv(path)
+path1 = 'data/ex1.txt'
+data = pd.read_csv(path1,sep='\t',names=['x0','x1','y'])  #读取用空格隔开的txt数据
+n=data.head()
+print(n)
 
+path2 = 'data/ex2data1.txt'
+data3 = pd.read_csv(path2,sep=',',names=['x0','x1','y'])  #读取用,隔开的txt数据
+m=data3.head()
+print(m)
+
+path3='data/regress_data1.csv'    #读取csv格式数据
+data4=pd.read_csv(path3)
+p=data4.head()
+print(p)
